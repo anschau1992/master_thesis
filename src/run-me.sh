b218d043d6b7035d6d6ba37eb1e5cad91c67a71a
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2019-05-26 14:03:28 +0200

    Update run-me.sh

--- src/run-me.sh
+++ src/run-me.sh
@@ -105,7 +105,7 @@ $MARIAN_DECODER \
 
 
 # make file with only one word TODO: find better way -> model should actually do this itself
-grep -Eo '^[^ ]+' test.trg.de.output > test.trg.de.output_one
+grep -Eo '^[^ ]+' test.trg.de.output > test.trg.de.output_one_word
 
 # calculate scores
 echo "Start of Score calculation"
