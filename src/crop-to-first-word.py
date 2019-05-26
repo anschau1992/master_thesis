95ef416da88d449ff4fff23a1fca7c5fee3d5c0c
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2019-05-26 17:22:14 +0200

    Update crop-to-first-word.py

--- src/crop-to-first-word.py
+++ src/crop-to-first-word.py
@@ -1,4 +1,4 @@
 with open('../data/test.trg.de.output', 'r') as input_file:
-    with open('../data/test.trg.de.output_one_name', 'w+') as output_file:
+    with open('../data/test.trg.de.output_one_word', 'w+') as output_file:
         for line in input_file:
-            output_file.write(line.split(' ', 1)[0])
+            output_file.write(line.split(' ', 1)[0] + '\n')
