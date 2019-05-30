a03fbdf3089009b734f73ab8c1c4697136c41b21
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2019-05-30 18:25:50 +0200

    Adapt download-script for autodesk
    - include auto-desk data set in the download script
    - preprocess autodesk data set --> extrac en and de text
    - include clean_up file for prepare new model-training run
    -include autodesk source data as .bz2-file

---
+++ src/scripts/clean_up.sh
@@ -0,0 +1,21 @@
+# Clean up after training and testing. Set everything up for a new round of Model-training
+#!/usr/bin/env bash
+
+# create new folder for the current approach
+i=1
+while [[ -d "../generated_approaches/$i" ]]
+do
+    i=$(( $i + 1))
+done
+mkdir -p "../generated_approaches/$i"
+
+
+# move and copy files
+mv ../model/train.log ../generated_approaches/${i}/train.log
+mv ../model/valid.log ../generated_approaches/${i}/valid.log
+mv ../model/test.log ../generated_approaches/${i}/test.log
+cp ../src/run-me.sh ../generated_approaches/${i}/run-me.sh
+
+# clean up & set up for new training round
+rm ../data/test.trg.de.output
+rm -r ../model
--- src/scripts/download-source-data.sh
+++ src/scripts/download-source-data.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env bash
 
 # Download and unzip the three corpia Ubuntu, OpenOffice and PHP from 'http://opus.nlpl.eu'
-# Prepare and preprocess data, merge all three corpia together
+# Prepare and pre-process data, merge all three corpia together with the local data from Autodesk (deu.mt.bz2).
 
-cd ..
-mkdir -p source_data
-cd source_data
+
+mkdir -p ../source_data
+cd ../source_data
 
 # get the three corpia
 wget -nc --output-document=ubuntu-de-en.txt.zip http://opus.nlpl.eu/download.php?f=Ubuntu/v14.10/moses/de-en.txt.zip
 wget -nc --output-document=oo-de-en.txt.zip http://opus.nlpl.eu/download.php?f=OpenOffice/v3/moses/de-en_GB.txt.zip
 wget -nc --output-document=php-de-en.txt.zip http://opus.nlpl.eu/download.php?f=PHP/v1/moses/de-en.txt.zip
-wget -nc --output-document=ted-de-en.txt.zip http://opus.nlpl.eu/download.php?f=TED2013/v1.1/moses/de-en.txt.zip
 
-unzip -o ubuntu-de-en.txt.zip -d ../source_data/ubuntu
-unzip -o oo-de-en.txt.zip -d ../source_data/oo
-unzip -o php-de-en.txt.zip -d ../source_data/php
-unzip -o ted-de-en.txt.zip -d ../source_data/ted
+unzip -o ubuntu-de-en.txt.zip -d ./ubuntu
+unzip -o oo-de-en.txt.zip -d ./oo
+unzip -o php-de-en.txt.zip -d ./php
 
-# Preprocess training data
+# unzip and pre-process Autodesk data
+bzip2 -dk ../deu.mt.bz2
+sed -i -e 's//$/g' ../deu.mt # replace multi-char symbol by single char, as 'cut' only works with it
+while read line
+do
+    A="$(cut -d'$' -f1 <<<"$line")"
+    B="$(cut -d'$' -f2 <<<"$line")"
+    echo "$A" >> autodesk.output.en
+    echo "$B" >> autodesk.output.de
+done < "../deu.mt"
+
+# Pre-process training data
 cd ../src
-python3 __init_generator__.py -vv -ien ../source_data/ubuntu/Ubuntu.de-en.en -ide ../source_data/ubuntu/Ubuntu.de-en.de -o ./data
-python3 __init_generator__.py -vv -ien ../source_data/oo/OpenOffice.de-en_GB.en_GB -ide ../source_data/oo/OpenOffice.de-en_GB.de -o ./data
-python3 __init_generator__.py -vv -ien ../source_data/php/PHP.de-en.en -ide ../source_data/php/PHP.de-en.de -o ./data
-python3 __init_generator__.py -vv -ien ../source_data/ted/TED2013.de-en.en -ide ../source_data/ted/TED2013.de-en.de -o ./data
+python3 __init_generator__.py -vv -ien ./ubuntu/Ubuntu.de-en.en -ide ../source_data/ubuntu/Ubuntu.de-en.de -o ./data
+python3 __init_generator__.py -vv -ien ./oo/OpenOffice.de-en_GB.en_GB -ide ../source_data/oo/OpenOffice.de-en_GB.de -o ./data
+python3 __init_generator__.py -vv -ien ./php/PHP.de-en.en -ide ../source_data/php/PHP.de-en.de -o ./data
+python3 __init_generator__.py -vv -ien ./autodesk.output.en -ide ./autodesk.output.de -o ./data
 
 cd ..
 rm -r -f source_data
+rm ./deu.mt
