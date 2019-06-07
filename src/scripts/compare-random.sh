1ff5ba9a12a5d63291dcc84a94b6cc8dcaf8159e
Andreas Schaufelbühl <andreas.schaufelbuehl@gmail.com>
2019-06-07 19:16:03 +0200

    add compare-bash script
    shows in a nice table random examples of a generated model

---
+++ src/scripts/compare-random.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+##
+## Print out random lines of the given model to compare the results with the source
+##
+## $1: folder-name in generated_approaches/ => e.g. 4
+## $2: number of lines to print
+
+
+
+LINE_LENGTH=$(wc -l < ../../generated_approaches/"$1"/test.src.de)
+
+let "RANDOM_NUMB= $RANDOM % ${LINE_LENGTH}"
+
+test_src_de=()
+test_trg_de=()
+test_trg_de_output=()
+test_src_en=()
+accuracy=()
+chrf=()
+
+
+test_var=$( [[ -z "$my_var" ]] && echo "NULL" || echo "Not NULL" )
+
+max_i=$(( ${RANDOM_NUMB}+$2 ))
+for i in `seq ${RANDOM_NUMB} ${max_i}`
+do
+    src_de=$(sed "${i}!d" "../../generated_approaches/"$1"/test.src.de")
+    test_src_de+=( "$( [[ -z "$src_de" ]] && echo "[NULL]" || echo ${src_de} )" )
+
+    trg_de=$(sed "${i}!d" "../../generated_approaches/"$1"/test.trg.de")
+    test_trg_de+=( "$( [[ -z "$trg_de" ]] && echo "[NULL]" || echo ${trg_de} )" )
+
+    trg_de_output=$(sed "${i}!d" "../../generated_approaches/"$1"/test.trg.de.output")
+    test_trg_de_output+=( "$( [[ -z "$trg_de_output" ]] && echo "[NULL]" || echo ${trg_de_output} )" )
+
+    src_en=$(sed "${i}!d" "../../generated_approaches/"$1"/test.src.en")
+    test_src_en+=( "$( [[ -z "$src_en" ]] && echo "[NULL]" || echo ${src_en} )" )
+
+    scoring_line=$(sed "${i}!d" "../../generated_approaches/"$1"/scoring.output")
+    accuracy+=( "$(echo ${scoring_line} | cut -d";" -f1)" )
+    chrf+=( "$(echo ${scoring_line} | cut -d";" -f2)" )
+done
+
+# Table formatting
+divider===========================================================
+divider=$divider$divider$divider$divider
+width=180
+
+
+header="\n %-10s %-25s %-30s %-30s %-15s %-15s %-20s\n"
+formatf=" %-10i %-25s %-30s %-30s %-15i %-15g %.80s\n"
+printf "$header" "#Line" "Source-DE" "Target-DE" "Generated-Output" "Accuracy" "Chrf" "Source-EN"
+printf "%$width.${width}s\n" "$divider"
+for i in `seq 0 $2`
+do
+    printf "$formatf" \
+    $(( ${RANDOM_NUMB}+i )) ${test_src_de[i]} ${test_trg_de[i]} ${test_trg_de_output[i]} ${accuracy[i]} ${chrf[i]} "${test_src_en[i]}"
+done
